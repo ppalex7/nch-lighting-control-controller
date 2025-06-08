09c38b2cce7d1d7f260891f29ccba26b7a782404
Aleksandr Duplishchev <2ppalex@gmail.com>
2025-06-08 15:49:17 +0300

    switch system clock source from HSI to HSE

---
+++ Inc/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+static inline void configure_sysclock(void);
+
+#endif
--- Src/main.c
+++ Src/main.c
@@ -1,11 +1,34 @@
-#include <stdint.h>
+#include "stm32f030x6.h"
+
+#include "main.h"
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-int main(void)
-{
-    /* Loop forever */
-	for(;;);
+int main(void) {
+
+    configure_sysclock();
+
+    while (1) {
+    }
+}
+
+/**
+ * Configure clock to 12 MHz fed from HSE
+ */
+static void configure_sysclock(void) {
+    // Enable High Speed External Clock
+    RCC->CR |= RCC_CR_HSEON;
+
+    // wait until HSE clock ready
+    while (!(RCC->CR & RCC_CR_HSERDY))
+        ;
+
+    // switch to HSE clock
+    RCC->CFGR = (RCC->CFGR & (~RCC_CFGR_SW)) | RCC_CFGR_SW_HSE;
+
+    // wait until clock switched
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSE)
+        ;
 }
